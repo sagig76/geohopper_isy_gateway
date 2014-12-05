a7f75b3ccf3ecca5b4521d442b39dabf038db19e
sagig76 <sagi.geva@icloud.com>
2014-12-04 21:40:31 -0800

    log and shell script files

---
+++ geohopper_isy_gateway.sh
@@ -0,0 +1,7 @@
+### Run Geohopper ISY Gateway (uncomment 2nd and comment 1st line to run in background)
+
+perl geohopper_isy_gateway.pl daemon --listen "http://*:9129"
+
+
+#perl isy_gateway.pl daemon --listen "http://*:9129" &>/dev/null &
+
