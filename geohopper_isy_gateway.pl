5c440a278e6375ad401500452f8c62c2c7550983
sagig76 <sagi.geva@icloud.com>
2014-12-05 22:12:05 -0800

    Version 0.4 with cfg support

--- geohopper_isy_gateway.pl
+++ geohopper_isy_gateway.pl
@@ -3,7 +3,7 @@
 #
 # Author:       Sagi Geva - sagi.geva@icloud.com
 # Project:      Geohopper ISY Home Automation Gateway
-# Version:      0.3
+# Version:      0.4
 # Date:			05 Dec 2014
 #
 
@@ -11,51 +11,11 @@
 use Mojolicious::Lite;
 use Data::Dumper;
 use LWP::UserAgent;
+use Config::Simple;
 
 
-### DEFINE ISY CONNECTION PARAMETERS ###
-
-my %isy_params = (
-	
-	'address' => '192.168.0.20',
-	'port' => '80',
-	'user' => 'admin',
-	'pass' => 'yourpassword',
-	
-);
-	
-
-### DEFINE GEOHOPPER LOCATION NAMES AND MATCHING ISY VAR IDS (LOCATION NAMES ARE CASE SENSITIVE) ###
-
-my %isy_locations = (
-	
-	'Master Bedroom' => '27',
-	'House Entrance' => '26',
-	'Car' => '42',
-	'Office' => '38',
-	
-);
-
-
-### DEFINE AUTHORIZED USER(S), ADD AS MANY AS YOU WANT ###
-
-my %authorized = (
-
-	1 => 'someone@icloud.com',
-	2 => 'someone2@gmail.com',
-		
-);
-
-
-### DEFINE VAR VALUES FOR EACH EVENT TYPE ###
-
-my %event_value = (
-
-	'Enter' => '1',
-	'Exit' => '0',
-	'Test' => '2',
-
-); 
+my %config;
+Config::Simple->import_from('geohopper_isy_gateway.cfg', \%config);
 
 
 ### UPDATE ISY VAR ###
@@ -63,8 +23,8 @@ my %event_value = (
 sub update_isy_var {
 
 	my $browser = LWP::UserAgent->new;
-	my $req =  HTTP::Request->new(GET => "http://$isy_params{'address'}:$isy_params{'port'}/rest/vars/set/2/$isy_locations{$_[0]}/$event_value{$_[1]}");
-	$req->authorization_basic("$isy_params{'user'}", "$isy_params{'pass'}");
+	my $req =  HTTP::Request->new(GET => "http://$config{'isy.address'}:$config{'isy.port'}/rest/vars/set/2/$config{\"location.$_[0]\"}/$config{\"event.$_[1]\"}");
+	$req->authorization_basic("$config{'isy.user'}", "$config{'isy.pass'}");
 	my $reply = $browser->request($req);
 	return $reply;
 
@@ -80,35 +40,39 @@ get '/' => sub {
 
 
 post '/geohopper' => sub {
-
-
-### PARSE GEOHOPPER MESSAGE ###
+	
+	
+	### PARSE GEOHOPPER MESSAGE ###
 
     my $self = shift;
     my $text = $self->req->text;
     $text =~ tr/"{}//d; 
     my @data = split(/,/, $text);
 	
-    $self->app->log->debug(@data);
+    $self->app->log->debug(%config);
 	
-    
     my $sender = substr($data[0], 7);
     my $location = substr($data[1], 9);
     my $event = substr($data[2], 14);
 	
-    
+	
+	    
 	### CHECK IF USER IS AUTHORIZED ###
 
-    if (grep {$sender eq $_} values %authorized)  {
+    if (grep {/$sender/i} values %config)  {
 
         $self->res->headers->header( 'Content-Type' => 'text/html' );
-
+		
+	    $self->app->log->info($config{"location.$location"});
+	    $self->app->log->info($config{"event.$event"});
+		
 
 		### VERIFY LOCATION AND UPDATE ISY VAR ###
        
-        if (grep {$location eq $_} keys %isy_locations) {
-
+        if (grep {/$location/i} keys %config) {
+			
 			update_isy_var($location,$event);
+			
             $self->app->log->info("User: $sender. Location: $location. Event: $event");        	
 			$self->render( text => 'OK', status => 200 );
 
@@ -133,5 +97,5 @@ post '/geohopper' => sub {
 };
 
 app->log->path('log/geohopper_isy_gateway.log');
-app->log->level('debug');
+app->log->level('info');
 app->start;
